0ea74a6fc3055ce077efd50d3af3a480c6932ee9
sostenesg7 <sostenesg7@gmail.com>
2021-07-03 04:26:54 -0300

    http connector

--- http-server/index.js
+++ http-server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-// app.use(express.json());
+app.use(
+  express.json({
+    verify: (req, res, buff) => {
+      console.log(req.read(250), buff);
+    },
+  })
+);
 const mongoose = require('mongoose');
 const { MONGO_URI, MONGO_USER, MONGO_PASS } = process.env;
 
